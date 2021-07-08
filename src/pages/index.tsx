f12621cecaacd4bb727a2d048e2f2ed18ad719cb
Pedro Brandão <pedro@significa.co>
2021-07-08 19:24:24 +0100

    fix: document title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import Head from 'next/head'
 
 import { App } from 'components/App'
 
-const IndexPage: React.FC = () => <App />
+const IndexPage: React.FC = () => (
+  <>
+    <Head>
+      <title>
+        Find good movies from the last 7 days of Portuguese television
+      </title>
+    </Head>
+    <App />
+  </>
+)
 
 export default IndexPage
