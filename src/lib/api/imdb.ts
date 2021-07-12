4c8967ce41502e2217398c8ffc4073ae5fb7fbac
Pedro Brandão <pedro@significa.co>
2021-07-12 15:21:08 +0100

    fix: imdb id

--- src/lib/api/imdb.ts
+++ src/lib/api/imdb.ts
@@ -25,7 +25,8 @@ const removeExtraStuff = (text: string) => {
 const getEndpointForTitle = (title: string) => {
   return (
     `/${title.charAt(0).toLowerCase()}/` +
-    removeExtraStuff(title.slice(0, 20).toLowerCase())
+    removeExtraStuff(title.toLowerCase())
+      .slice(0, 20)
       .normalize('NFD')
       .replace(/\p{Diacritic}/gu, '')
       .replaceAll(' ', '_') +
