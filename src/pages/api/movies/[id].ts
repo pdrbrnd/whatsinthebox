65f24b127da020419c37b82ac71b8cb7abf4fcee
Pedro Revez da Silva <pedro.revez.silva@gmail.com>
2021-09-23 21:25:37 +0100

    Add where type

--- src/pages/api/movies/[id].ts
+++ src/pages/api/movies/[id].ts
@@ -1,10 +1,21 @@
 import { withSentry, captureException } from '@sentry/nextjs'
+import dayjs from 'dayjs'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 import { fetchGraphql } from 'lib/graphql'
 
 export default withSentry(async (req: NextApiRequest, res: NextApiResponse) => {
   const { id } = req.query
+  const where = {
+    _and: [
+      {
+        start_time: {
+          _gte: dayjs().subtract(7, 'days').format('YYYY-MM-DD HH:mm'),
+        },
+      },
+      { start_time: { _lte: dayjs().format('YYYY-MM-DD HH:mm') } },
+    ],
+  } as const
 
   if (!id || typeof id !== 'string') {
     return res.status(401).json({ error: 'Missing ID' })
@@ -15,13 +26,12 @@ export default withSentry(async (req: NextApiRequest, res: NextApiResponse) => {
       {
         movies_by_pk: Record<string, unknown>
       },
-      { imdbId: string }
+      { imdbId: string; where: typeof where }
     >({
       query: `
-      query getMovie($where: movies_bool_exp!, $imdbId: String!) {
+      query getMovie($where: schedules_bool_exp!, $imdbId: String!) {
         movies_by_pk(
-          imdb_id: $imdbId, 
-          where: $where
+          imdb_id: $imdbId
           ) {
           title
           actors
@@ -38,7 +48,7 @@ export default withSentry(async (req: NextApiRequest, res: NextApiResponse) => {
           runtime
           year
           writer
-          schedules {
+          schedules(where: $where) {
             start_time
             title
             channel {
@@ -50,19 +60,7 @@ export default withSentry(async (req: NextApiRequest, res: NextApiResponse) => {
       `,
       variables: {
         imdbId: id,
-        where: {
-          _and: filters,
-          schedules: {
-            _and: [
-              {
-                start_time: {
-                  _gte: dayjs().subtract(7, 'days').format('YYYY-MM-DD HH:mm'),
-                },
-              },
-              { start_time: { _lte: dayjs().format('YYYY-MM-DD HH:mm') } },
-            ],
-          },
-        },
+        where,
       },
     })
 
