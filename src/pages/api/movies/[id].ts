969d0232ded56392e42bd960a066f57e27ca554d
Pedro Silva <pedro.revez.silva@gmail.com>
2021-09-24 17:55:45 +0100

    Update formatting

--- src/pages/api/movies/[id].ts
+++ src/pages/api/movies/[id].ts
@@ -5,30 +5,30 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { fetchGraphql } from 'lib/graphql'
 
 export default withSentry(async (req: NextApiRequest, res: NextApiResponse) => {
-    const { id } = req.query
-    const where = {
-        _and: [
-            {
-                start_time: {
-                    _gte: dayjs().subtract(7, 'days').format('YYYY-MM-DD HH:mm'),
-                },
-            },
-            { start_time: { _lte: dayjs().format('YYYY-MM-DD HH:mm') } },
-        ],
-    } as const
-
-    if (!id || typeof id !== 'string') {
-        return res.status(401).json({ error: 'Missing ID' })
-    }
-
-    try {
-        const { data, errors } = await fetchGraphql <
-        {
-            movies_by_pk: Record < string, unknown>
+  const { id } = req.query
+  const where = {
+    _and: [
+      {
+        start_time: {
+          _gte: dayjs().subtract(7, 'days').format('YYYY-MM-DD HH:mm'),
+        },
       },
-    { imdbId: string; where: typeof where }
-    > ({
-        query: `
+      { start_time: { _lte: dayjs().format('YYYY-MM-DD HH:mm') } },
+    ],
+  } as const
+
+  if (!id || typeof id !== 'string') {
+    return res.status(401).json({ error: 'Missing ID' })
+  }
+
+  try {
+    const { data, errors } = await fetchGraphql<
+      {
+        movies_by_pk: Record<string, unknown>
+      },
+      { imdbId: string; where: typeof where }
+    >({
+      query: `
       query getMovie($where: schedules_bool_exp!, $imdbId: String!) {
         movies_by_pk(
           imdb_id: $imdbId
@@ -58,22 +58,22 @@ export default withSentry(async (req: NextApiRequest, res: NextApiResponse) => {
         }
       }
       `,
-        variables: {
-            imdbId: id,
-            where,
-        },
+      variables: {
+        imdbId: id,
+        where,
+      },
     })
 
     if (!data || errors) {
-        throw {
-            message: `Could not fetch movie ${id}`,
-            errors: errors || [],
-        }
+      throw {
+        message: `Could not fetch movie ${id}`,
+        errors: errors || [],
+      }
     }
 
     res.status(200).json({ details: data ? data.movies_by_pk : null })
-} catch (error) {
+  } catch (error) {
     captureException(error)
     res.status(401).json({ error })
-}
+  }
 })
