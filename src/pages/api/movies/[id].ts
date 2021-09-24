3cf1f3b16eb0af75bd70080f269b0af5992b89e3
Pedro Silva <pedro.revez.silva@gmail.com>
2021-09-24 10:30:56 +0100

    fix: Add where type

--- src/pages/api/movies/[id].ts
+++ src/pages/api/movies/[id].ts
@@ -1,25 +1,38 @@
 import { withSentry, captureException } from '@sentry/nextjs'
+import dayjs from 'dayjs'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 import { fetchGraphql } from 'lib/graphql'
 
 export default withSentry(async (req: NextApiRequest, res: NextApiResponse) => {
-  const { id } = req.query
+    const { id } = req.query
+    const where = {
+        _and: [
+            {
+                start_time: {
+                    _gte: dayjs().subtract(7, 'days').format('YYYY-MM-DD HH:mm'),
+                },
+            },
+            { start_time: { _lte: dayjs().format('YYYY-MM-DD HH:mm') } },
+        ],
+    } as const
 
-  if (!id || typeof id !== 'string') {
-    return res.status(401).json({ error: 'Missing ID' })
-  }
+    if (!id || typeof id !== 'string') {
+        return res.status(401).json({ error: 'Missing ID' })
+    }
 
-  try {
-    const { data, errors } = await fetchGraphql<
-      {
-        movies_by_pk: Record<string, unknown>
+    try {
+        const { data, errors } = await fetchGraphql <
+        {
+            movies_by_pk: Record < string, unknown>
       },
-      { imdbId: string }
-    >({
-      query: `
-      query getMovie($imdbId: String!) {
-        movies_by_pk(imdb_id: $imdbId) {
+    { imdbId: string; where: typeof where }
+    > ({
+        query: `
+      query getMovie($where: schedules_bool_exp!, $imdbId: String!) {
+        movies_by_pk(
+          imdb_id: $imdbId
+          ) {
           title
           actors
           country
@@ -35,7 +48,7 @@ export default withSentry(async (req: NextApiRequest, res: NextApiResponse) => {
           runtime
           year
           writer
-          schedules {
+          schedules(where: $where) {
             start_time
             title
             channel {
@@ -45,34 +58,22 @@ export default withSentry(async (req: NextApiRequest, res: NextApiResponse) => {
         }
       }
       `,
-      variables: {
-        imdbId: id,
-      },
-      where: {
-        _and: filters,
-        schedules: {
-          _and: [
-            {
-              start_time: {
-                _gte: dayjs().subtract(7, 'days').format('YYYY-MM-DD HH:mm'),
-              },
-            },
-            { start_time: { _lte: dayjs().format('YYYY-MM-DD HH:mm') } },
-          ],
+        variables: {
+            imdbId: id,
+            where,
         },
-      },
     })
 
     if (!data || errors) {
-      throw {
-        message: `Could not fetch movie ${id}`,
-        errors: errors || [],
-      }
+        throw {
+            message: `Could not fetch movie ${id}`,
+            errors: errors || [],
+        }
     }
 
     res.status(200).json({ details: data ? data.movies_by_pk : null })
-  } catch (error) {
+} catch (error) {
     captureException(error)
     res.status(401).json({ error })
-  }
+}
 })
